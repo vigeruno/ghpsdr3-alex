19b0c75c2daf64311078bf0e53a270faaf2288ed
vigeruno <viger-uno@tiscali.it>
2016-10-21 17:58:38 +0200

    Update soundcard.c
    FIX SMETER SDR-IQ

--- trunk/src/dspserver/soundcard.c
+++ trunk/src/dspserver/soundcard.c
@@ -80,8 +80,8 @@ fprintf(stderr,"setSoundcard: %d\n",card);
         case SDR_IQ:
             //multimeterCalibrationOffset=-240.0f - 25.0f;
             //displayCalibrationOffset=-18.0f;
-            multimeterCalibrationOffset=-240.0f - 25.0f;
-            displayCalibrationOffset=-240.0f - 18.0f;
+             multimeterCalibrationOffset=-240.0f - 0.0f; //-240.0f - 25.0f
+             displayCalibrationOffset=-240.0f - 18.0f;   //-240.0f - 18.0f
             break;
         case U7_KX3:
             // G0HWW's figs - XG3 @ -73dBm for S9
